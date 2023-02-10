9762284305d9a97f8d54c82ca17a645c6bfc2e50
Junaid Hussain <47213023+junnhssn@users.noreply.github.com>
2023-02-10 22:21:41 +0530

    Update psscript-dt-test.ps1

--- MD-100/scripts/psscript-dt-test.ps1
+++ MD-100/scripts/psscript-dt-test.ps1
@@ -8,6 +8,8 @@ Param (
 )
 
 Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
+[Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" 
 
 #Reset VM password to update it to random password, it is a custom image based VM
 net user $adminUsername $adminPassword
