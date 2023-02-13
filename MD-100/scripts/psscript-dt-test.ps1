3f162ac0a91147b05242f5eaec117250acedaefd
Junaid Hussain <47213023+junnhssn@users.noreply.github.com>
2023-02-13 13:29:03 +0530

    Update psscript-dt-test.ps1

--- MD-100/scripts/psscript-dt-test.ps1
+++ MD-100/scripts/psscript-dt-test.ps1
@@ -1,15 +1,41 @@
 Param (
     [Parameter(Mandatory = $true)]
     [string]
+    $AzureUserName,
+
+    [string]
+    $AzurePassword,
+
+    [string]
+    $AzureTenantID,
+
+    [string]
+    $AzureSubscriptionID,
+
+    [string]
+    $ODLID,
+
+    [string]
+    $DeploymentID,
+
+    [string]
     $adminUsername,
 
     [string]
     $adminPassword
 )
-
-Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
 [Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" 
+Start-Transcript -Path C:\WindowsAzure\Logs\CloudLabsCustomScriptExtension.txt -Append
+
+#Import Common Functions
+$path = pwd
+$path=$path.Path
+$commonscriptpath = "$path" + "\cloudlabs-common\cloudlabs-windows-functions.ps1"
+. $commonscriptpath
+
+# Run Imported functions from cloudlabs-windows-functions.ps1
+CreateCredFile $AzureUserName $AzurePassword $AzureTenantID $AzureSubscriptionID $DeploymentID $ODLID
 
 #Reset VM password to update it to random password, it is a custom image based VM
 net user $adminUsername $adminPassword
